d7ffa940d526301ba28f2a9027a798c776e07b12
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-08-30 13:30:46 +0800

    Refactor state to remove duplication
    - remove state that can be computed from other state
    - prevent first click from being a mine
    - explode the clicked mine and end the game
    - uncover all mines when game is lost

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,30 @@
 /*
 # TODO:
   - [x] timer
-  - [ ] mine counter
+  - [x] mine counter
   - [x] reset game
   - [x] flagging a tile
   - [x] clicking on a tile, revealing it if it's not flagged
   - [x] display number of surrounding mines on an uncovered tile
-  - [ ] recursively uncovering all tiles
-  - [ ] prevent first click from being a mine
-  - [ ] mine counter
+  - [x] consolodate state that can be computed from other state
+  - [x] uncover all mines when game is lost
+  - [x] explode when you click on a mine
+  - [x] prevent first click from being a mine
+  - [x] count mines minus flags placed
+  - [ ] recursively uncovering all tiles when clicking on a tile with no surrounding mines
+  - [ ] stop timer when game is over
   - [ ] win state
+  - [ ] remove enums?
 */
 
 import { useReducer, useRef, useState } from 'react'
 import { clsx } from 'clsx'
+
+interface Dificulty {
+  rows: number
+  cols: number
+  mines: number
+}
 
 const LEVELS = {
   beginner: {
@@ -31,7 +42,9 @@ const LEVELS = {
     cols: 30,
     mines: 99,
   },
-}
+} as const
+
+type LevelName = keyof typeof LEVELS
 
 enum GameState {
   NEW,
@@ -39,6 +52,12 @@ enum GameState {
   WON,
   LOST,
 }
+// create the type from an enum:
+// const GAME_STATES = ['new', 'playing', 'won', 'lost'] as const
+// type GameState = typeof GAME_STATES[number]
+
+// or just create a type directly:
+// type GameState = 'new' | 'playing' | 'won' | 'lost'
 
 enum TileState {
   COVERED,
@@ -47,26 +66,52 @@ enum TileState {
   EXPLODED,
 }
 
-type Tile = {
-  row: number
-  col: number
-  surroundingMines: number
-  isMine: boolean
-  state: TileState
+type Board = TileState[][]
+type Coords = readonly [number, number]
+type CoordsMap = Map<string, Coords>
+
+interface State {
+  gameState: GameState
+  board: Board
+  width: number
+  height: number
+  mineCoords: CoordsMap
 }
 
-function createMineSet(rows: number, cols: number, mines: number) {
-  const mineSet = new Set<string>()
-  while (mineSet.size < mines) {
-    mineSet.add(
-      `${Math.floor(Math.random() * rows)},${Math.floor(Math.random() * cols)}`,
-    )
+enum ActionType {
+  RESTART,
+  UNCOVER,
+  TOGGLE_FLAG,
+}
+
+interface Action {
+  type: ActionType
+  coords?: Coords
+}
+
+function createMineMap(
+  width: number,
+  height: number,
+  mines: number,
+  avoid?: Coords,
+): CoordsMap {
+  const mineMap = new Map() as CoordsMap
+  while (mineMap.size < mines) {
+    const mineCoords: Coords = [
+      Math.floor(Math.random() * width),
+      Math.floor(Math.random() * height),
+    ]
+    mineMap.set(mineCoords.join(), mineCoords)
   }
-  return mineSet
+  if (avoid && mineMap.has(avoid.join())) {
+    return createMineMap(width, height, mines, avoid)
+  }
+  return mineMap
 }
 
-function surroundingMines(row: number, col: number, mineSet: Set<string>) {
-  const surrounding = [
+function surroundingCoords(coords: Coords) {
+  const [row, col] = coords
+  return [
     [row - 1, col - 1],
     [row - 1, col],
     [row - 1, col + 1],
@@ -76,125 +121,158 @@ function surroundingMines(row: number, col: number, mineSet: Set<string>) {
     [row + 1, col],
     [row + 1, col + 1],
   ]
-  return surrounding.reduce((acc, [r, c]) => {
-    if (mineSet.has(`${r},${c}`)) {
+}
+
+function getSurroundingMineCount(startingCoords: Coords, mineSet: CoordsMap) {
+  const surrounding = surroundingCoords(startingCoords)
+  return surrounding.reduce((acc, coords) => {
+    if (mineSet.has(coords.join())) {
       return acc + 1
     }
     return acc
   }, 0)
 }
 
-function createGameBoard({
-  rows,
-  cols,
-  mineCoords,
-}: {
-  rows: number
-  cols: number
-  mineCoords: Set<string>
-}) {
-  return Array(rows)
-    .fill(null)
-    .map((_, row) => {
-      return Array(cols)
-        .fill(null)
-        .map((_, col) => {
-          return {
-            row,
-            col,
-            surroundingMines: surroundingMines(row, col, mineCoords),
-            isMine: mineCoords.has(`${row},${col}`),
-            state: TileState.COVERED,
-          }
-        })
-    })
-}
-
-enum ActionType {
-  RESTART = 'RESTART',
-  UNCOVER = 'UNCOVER',
-  TOGGLE_FLAG = 'TOGGLE_FLAG',
-}
-type Coords = {
-  row: number
-  col: number
-}
-type Action = {
-  type: ActionType
-  coords?: Coords
-}
-
-type State = {
-  gameState: GameState
-  board: Tile[][]
-  width: number
-  height: number
-  mineCount: number
-  flagCount: number
-  uncoveredCellCount: number
-}
-
-function initState(action?: Action): State {
-  const { rows, cols, mines } = LEVELS.beginner
-  const mineCoords = createMineSet(rows, cols, mines)
+function initState(initArg: { dificulty: Dificulty; avoid?: Coords }): State {
+  const { rows, cols, mines } = initArg.dificulty
+  const mineCoords = createMineMap(rows, cols, mines, initArg.avoid)
   return {
-    board: createGameBoard({ rows, cols, mineCoords }),
-    gameState: GameState.NEW,
+    board: Array(rows)
+      .fill(null)
+      .map((_, rowIndex) =>
+        Array(cols)
+          .fill(TileState.COVERED)
+          .map((tile, colIndex) => {
+            if (
+              initArg.avoid &&
+              initArg.avoid[0] === rowIndex &&
+              initArg.avoid[1] === colIndex
+            ) {
+              return TileState.UNCOVERED
+            } else {
+              return tile
+            }
+          }),
+      ),
+    gameState: initArg.avoid ? GameState.PLAYING : GameState.NEW,
     width: rows,
     height: cols,
-    mineCount: mineCoords.size,
-    flagCount: 0,
-    uncoveredCellCount: 0,
+    mineCoords,
   }
 }
-function updateBoard(prevBoard: Tile[][], coords: Coords) {
-  const newBoard = clone(prevBoard)
-  newBoard[coords.row][coords.col].state = TileState.UNCOVERED
-  return newBoard
+function uncoverTiles(
+  board: Board,
+  coords: Coords,
+  mineCoords: CoordsMap,
+): Board {
+  // uncover the tile we clicked on
+  return board.map((row, rowIndex) =>
+    row.map((tile, colIndex) => {
+      if (rowIndex === coords[0] && colIndex === coords[1]) {
+        return TileState.UNCOVERED
+      } else {
+        return tile
+      }
+      // then if there are no mines neibouring the tile, uncover all surrounding tiles
+      // continue uncovering neibour tiles until we find all mines
+    }),
+  )
+}
+
+// function recursivelyUncoverTiles(state: State, coords: Coords): State {
+//   const { board } = state
+//   if (surroundingMines(coords, state.mineCoords) === 0) {
+//     const surrounding = surroundingCoords(coords)
+//     for (const [r, c] of surrounding) {
+//       // todo...
+//       if (board[r][c].state === TileState.COVERED) {
+//         board = uncoverTiles(board, { row: r, col: c })
+//         board = recursivelyInitState(state, { row: r, col: c })
+//       }
+//     }
+//   }
+//   return { ...state, board }
+// }
+
+function getStateOnUncover(state: State, coords: Coords): State {
+  const { gameState, board, mineCoords } = state
+  switch (gameState) {
+    // if the game has already been won or lost, do nothing
+    case GameState.LOST:
+    case GameState.WON:
+      return state
+
+    case GameState.NEW:
+      // if it's a mine, reset the board
+      if (mineCoords.has(coords.join())) {
+        return {
+          ...initState({ dificulty: LEVELS.beginner, avoid: coords }),
+        }
+      }
+      // else uncover the tile
+      return {
+        ...state,
+        gameState: GameState.PLAYING,
+        board: uncoverTiles(board, coords, mineCoords),
+      }
+
+    case GameState.PLAYING:
+      // if we uncover a mine, explode the clicked mine and uncover all other mines
+      if (mineCoords.has(coords.join())) {
+        return {
+          ...state,
+          board: board.map((row, rowIndex) => {
+            return row.map((tile, colIndex) => {
+              if (rowIndex === coords[0] && colIndex === coords[1]) {
+                return TileState.EXPLODED
+              } else if (mineCoords.has([rowIndex, colIndex].join())) {
+                return TileState.UNCOVERED
+              } else {
+                return tile
+              }
+            })
+          }),
+          // and set the game state to lost
+          gameState: GameState.LOST,
+        }
+      } else {
+        // start uncovering tiles
+        return {
+          ...state,
+          board: uncoverTiles(board, coords, mineCoords),
+        }
+        // TODO: check if the game is won
+      }
+  }
 }
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
     case ActionType.RESTART:
-      return initState()
+      return initState({ dificulty: LEVELS.beginner })
     case ActionType.UNCOVER:
-      return {
-        ...state,
-        board:
-          action.coords != null
-            ? updateBoard(state.board, action.coords)
-            : state.board,
-        uncoveredCellCount: state.uncoveredCellCount + 1,
-        gameState: GameState.PLAYING,
-      }
-    case ActionType.TOGGLE_FLAG:
+      return action.coords != null
+        ? getStateOnUncover(state, action.coords)
+        : state
+    case ActionType.TOGGLE_FLAG: {
+      const coords = action.coords as Coords
       return {
         ...state,
         board: state.board.map((row, rowIndex) => {
           return row.map((tile, colIndex) => {
-            if (
-              rowIndex === action.coords?.row &&
-              colIndex === action.coords?.col
-            ) {
-              if (tile.state === TileState.FLAGGED) {
-                return { ...tile, state: TileState.COVERED }
+            if (rowIndex === coords[0] && colIndex === coords[1]) {
+              if (tile === TileState.FLAGGED) {
+                return TileState.COVERED
               } else {
-                return { ...tile, state: TileState.FLAGGED }
+                return TileState.FLAGGED
               }
             } else {
               return tile
             }
           })
         }),
-        flagCount:
-          action.coords != null
-            ? state.flagCount +
-              (state.board[action.coords.row][action.coords.col].state ===
-              TileState.FLAGGED
-                ? -1
-                : 1)
-            : state.flagCount,
       }
+    }
     default:
       console.error('unknown action', action)
       return state
@@ -204,13 +282,22 @@ function reducer(state: State, action: Action): State {
 function App() {
   const [state, dispatch] = useReducer(
     reducer,
-    { type: ActionType.RESTART },
+    { dificulty: LEVELS.beginner },
     initState,
   )
 
   const [time, setTime] = useState(0)
 
   const timerRef = useRef<number>()
+
+  const placedFlaggs = state.board.reduce((acc, row) => {
+    return (
+      acc +
+      row.reduce((acc, tile) => {
+        return tile === TileState.FLAGGED ? acc + 1 : acc
+      }, 0)
+    )
+  }, 0)
 
   function resetBoard() {
     dispatch({ type: ActionType.RESTART })
@@ -231,8 +318,8 @@ function App() {
       }, 1000)
     }
     // only allow clicks on covered tiles
-    const tile = state.board[coords.row][coords.col]
-    if (tile.state !== TileState.COVERED) return
+    const tile = state.board[coords[0]][coords[1]]
+    if (tile !== TileState.COVERED) return
     dispatch({ type: ActionType.UNCOVER, coords })
   }
 
@@ -241,9 +328,9 @@ function App() {
     // if the game is over, do nothing
     if (state.gameState === GameState.LOST || state.gameState == GameState.WON)
       return
-    const tile = state.board[coords.row][coords.col]
+    const tile = state.board[coords[0]][coords[1]]
     // don't do anything if tile is already revealed
-    if (tile.state === TileState.UNCOVERED) return
+    if (tile === TileState.UNCOVERED) return
     dispatch({ type: ActionType.TOGGLE_FLAG, coords })
   }
   return (
@@ -256,7 +343,7 @@ function App() {
         <div>{state.gameState === GameState.LOST && 'lost'}</div>
         <div className="flex justify-between">
           <div className="w-16 text-left">
-            {state.mineCount - state.flagCount}
+            {state.mineCoords.size - placedFlaggs}
           </div>
           <button className="text-6xl" onClick={resetBoard}>
             {state.gameState === GameState.NEW && '😀'}
@@ -272,9 +359,16 @@ function App() {
               {row.map((tile, colIndex) => (
                 <GameTile
                   key={`${rowIndex},${colIndex}`}
-                  tile={tile}
+                  x={rowIndex}
+                  y={colIndex}
                   onTileClick={handleClick}
                   onTileContextMenu={handleContextMenu}
+                  tileState={tile}
+                  isMine={state.mineCoords.has([rowIndex, colIndex].join())}
+                  surroundingMines={getSurroundingMineCount(
+                    [rowIndex, colIndex],
+                    state.mineCoords,
+                  )}
                 />
               ))}
             </div>
@@ -285,40 +379,41 @@ function App() {
   )
 }
 
-function GameTile({
-  tile,
-  onTileClick,
-  onTileContextMenu,
-}: {
-  tile: Tile
-  onTileClick: (e: React.MouseEvent, coords: Coords) => void
-  onTileContextMenu: (e: React.MouseEvent, coords: Coords) => void
-}) {
+type TileClickHandler = (e: React.MouseEvent, coords: Coords) => void
+interface GameTileProps {
+  x: number
+  y: number
+  tileState: TileState
+  onTileClick: TileClickHandler
+  onTileContextMenu: TileClickHandler
+  isMine: boolean
+  surroundingMines: number
+}
+
+function GameTile(props: GameTileProps) {
   return (
     <button
       className={clsx(
         {
           'bg-gray-400':
-            tile.state === TileState.COVERED ||
-            tile.state === TileState.FLAGGED,
-          'bg-gray-200': tile.state === TileState.UNCOVERED,
-          'bg-red-500': tile.state === TileState.EXPLODED,
+            props.tileState === TileState.COVERED ||
+            props.tileState === TileState.FLAGGED,
+          'bg-gray-200': props.tileState === TileState.UNCOVERED,
+          'bg-red-500': props.tileState === TileState.EXPLODED,
         },
         'h-8 w-8  border border-black',
       )}
-      onClick={(e) => onTileClick(e, { row: tile.row, col: tile.col })}
-      onContextMenu={(e) =>
-        onTileContextMenu(e, { row: tile.row, col: tile.col })
-      }
+      onClick={(e) => props.onTileClick(e, [props.x, props.y])}
+      onContextMenu={(e) => props.onTileContextMenu(e, [props.x, props.y])}
     >
-      {tile.state === TileState.COVERED && tile.isMine && 'x'}
-      {tile.state === TileState.FLAGGED && '🚩'}
-      {tile.state === TileState.EXPLODED && '💥'}
-      {tile.state === TileState.UNCOVERED &&
-        (tile.isMine
+      {props.tileState === TileState.COVERED && props.isMine && 'x'}
+      {props.tileState === TileState.FLAGGED && '🚩'}
+      {props.tileState === TileState.EXPLODED && '💥'}
+      {props.tileState === TileState.UNCOVERED &&
+        (props.isMine
           ? '💣'
-          : tile.surroundingMines > 0 && (
-              <span className="text-blue-700">{tile.surroundingMines}</span>
+          : props.surroundingMines > 0 && (
+              <span className="text-blue-700">{props.surroundingMines}</span>
             ))}
     </button>
   )
